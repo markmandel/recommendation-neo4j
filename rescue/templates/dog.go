cd2166a0167f29b726f26d5fa47fc6f481f2fa13
Mark Mandel <mark.mandel@gmail.com>
2015-02-05 09:04:50 +1100

    Final touches on the dog page.

--- rescue/templates/dog.go
+++ rescue/templates/dog.go
@@ -16,10 +16,15 @@ const Dog = `
 	</div>
 	<div class="row">
 		<div class="col-sm-3">
+			<p>
+				<a href="https://www.google.com/search?q=local+dog+adoption" target="_blank"
+				   onclick="javascript: window.alert('Unfortunately this is a fake dog adoption site.\nIf you are interested in adopting a dog, please check your local shelters.')"
+				   type="button" class="btn btn-success btn-lg btn-block">Adopt This Dog</a>
+			</p>
 			<p>{{.dog.Breed.Description}}</p>
 		</div>
 		<div class="col-sm-9">
-			<img src="{{.dog.PicURL}}"/>
+			<img class="img-responsive" src="{{.dog.PicURL}}"/>
 		</div>
 	</div>
 
